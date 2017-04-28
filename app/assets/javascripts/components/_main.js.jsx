d4f5a70705fc0b4aaf4249a007113f53097e48f2
astrung <tuananh93nguyen@gmail.com>
2017-04-28 15:29:12 +0700

    First

---
+++ app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,19 @@
+var Main = React.createClass({
+    render() {
+        return (
+            <div>
+                <Header />
+                <Body />
+            </div>
+        )
+    }
+});
+// var Main = React.createClass({
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Hello, World!</h1>
+//             </div>
+//         )
+//     }
+// });
